9a9c4ed2bc83ccd83b99b1fe4b1a75eb3bdac9a5
Artyom Gagin <37769598+Tesorp1X@users.noreply.github.com>
2021-05-23 23:34:53 +0700

    WIP
    WIP

--- bot/handlers/callbacks.py
+++ bot/handlers/callbacks.py
@@ -46,14 +46,14 @@ async def send_response_or_discussion_poll(call: types.CallbackQuery, callback_d
     user_id = callback_data["user_id"]
     problem_id = callback_data["problem_id"]
     problem_obj = queston_service.get_problem_by_id(problem_id)
-    url = None
+    
     if problem_obj.type == 'question':
         message = "Обсудить или ответить?"
     else:
         message = 'С помощью кнопки "Перейти к обсуждению" ты можешь присоединиться к телеграм-чату, посвященному этому проблеме'
-        url = problem_obj.invite_link
+        
     await call.message.answer(message,
-                              reply_markup=inline_kb.get_resp_or_disc_inline_kb(problem_obj, user_id, url))
+                              reply_markup=inline_kb.get_resp_or_disc_inline_kb(problem_obj, user_id)
     
     await call.answer()
 
