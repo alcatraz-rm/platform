760e0d0883045c6432a2c46e43db0a6495d8b2dc
German Yakimov <alcatraz@fctools.com>
2021-05-31 15:32:02 +0700

    Add database port to envs.

--- bot/db/base.py
+++ bot/db/base.py
@@ -1,7 +1,7 @@
 from peewee import *
 from playhouse.migrate import *
 
-from bot.config import DATABASE_PASSWORD, DATABASE_NAME, DATABASE_HOST, DATABASE_USER
+from bot.config import DATABASE_PASSWORD, DATABASE_NAME, DATABASE_HOST, DATABASE_USER, DATABASE_PORT
 
 
 db = PostgresqlDatabase(
@@ -9,6 +9,7 @@ db = PostgresqlDatabase(
     user=DATABASE_USER,
     password=DATABASE_PASSWORD,
     host=DATABASE_HOST,
+    port=DATABASE_PORT
 )
 
 migrator = PostgresqlMigrator(db)
