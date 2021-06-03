07c016e92b560ef3685ae0ec6879a1ba89f13801
Tesorp1X <rakteam2014@gmail.com>
2021-06-03 15:56:46 +0700

    HotFix: exception handling on DB savings (in account_service). TODO: do the same in question_service.

--- bot/handlers/messages.py
+++ bot/handlers/messages.py
@@ -1,8 +1,6 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 
-from peewee import InternalError
-
 import bot.keyboards.replay as kb
 from bot import constants
 from bot.config import dp, ADMINS_IDS
@@ -103,23 +101,15 @@ async def registration_complete(message: types.Message, state: FSMContext):
         telegram_data = message.from_user
         user_data = await state.get_data()
         user_data['degree_level'] = message.text
-        try:
-            account_service.add_new_user(t_id=telegram_data.id,
-                                         t_username=telegram_data.username,
-                                         name=user_data['name'],
-                                         email=user_data['email'],
-                                         department=user_data['department'],
-                                         degree_level=user_data['degree_level'],
-                                         )
 
-        except InternalError as ex:
-            account_service.add_new_user(t_id=telegram_data.id,
-                                         t_username=telegram_data.username,
-                                         name=user_data['name'],
-                                         email=user_data['email'],
-                                         department=user_data['department'],
-                                         degree_level=user_data['degree_level'],
-                                         )
+        account_service.add_new_user(t_id=telegram_data.id,
+                                     t_username=telegram_data.username,
+                                     name=user_data['name'],
+                                     email=user_data['email'],
+                                     department=user_data['department'],
+                                     degree_level=user_data['degree_level'],
+                                     )
+
         await message.answer("Что вам интересно? Сначала выберите науку, а затем предмет",
                              reply_markup=kb.get_science_list_km())
         await RegistrationProcessStates.waiting_for_interests_science.set()
