1e31d524ed85c0c23a051e226b778cb8595e2cc7
German Yakimov <alcatraz@fctools.com>
2021-06-03 23:17:57 +0700

    Fix texts.

--- bot/constants.py
+++ bot/constants.py
@@ -60,7 +60,7 @@ HELP_MESSAGE = """*Доступные команды:*
 
 FRIENDS_MESSAGE = """*Наши друзья:*
 
-1. Новое из старого - https://vk.com/novoe_iz_starogo
+1. Новое из старого - https://vk.com/novoe\_iz_\starogo
 """
 
 REGISTRATION_EXIT_SENTENCES = "Введите /exit, чтобы прервать регистрацию"
